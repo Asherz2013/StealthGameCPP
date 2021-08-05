567fcbea013dafdefebe5a95aa9bb6f27531c83b
Ashley <ashley.shaw2009@gmail.com>
2021-08-05 10:04:33 +0100

    Initial commit

---
+++ Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSObjectiveActor.h"
+
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "FPSCharacter.h"
+
+// Sets default values
+AFPSObjectiveActor::AFPSObjectiveActor()
+{
+	// Create a Static Mesh Component
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	// Turn off its collision
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	// Create a Sphere Component
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	// Setup Collision to Query Only
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	// Ignore EVERYTHING
+	SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	// Apart from the Pawn
+	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	// Make it a Child of the Static Mesh Component
+	SphereComp->SetupAttachment(MeshComp);
+
+	// Make the Static Mesh Component the ROOT component
+	RootComponent = MeshComp;
+}
+
+// Called when the game starts or when spawned
+void AFPSObjectiveActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AFPSObjectiveActor::PlayerEffects()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), PickupFX, GetActorLocation());
+}
+
+void AFPSObjectiveActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	PlayerEffects();
+
+	AFPSCharacter* MyCharacter = Cast<AFPSCharacter>(OtherActor);
+	if(MyCharacter)
+	{
+		MyCharacter->bIsCarryObjective = true;
+
+		Destroy();
+	}
+}
+
